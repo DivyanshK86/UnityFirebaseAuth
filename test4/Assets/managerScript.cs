e501f46d9a5e0ad6c10bb8f4781a06fa9e351b2a
DivyanshK86 <divyanshk86@gmail.com>
2018-07-16 16:49:06 +0530

    google sign in sucessful

---
+++ test4/Assets/managerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class managerScript : MonoBehaviour {
+
+    public SpriteRenderer squareSprite;
+    public Text errorTxt;
+
+    string WEB_CLIENT_ID = "129044841185-5s9p870updhpblbn4331e9g0reubdp1c.apps.googleusercontent.com";
+
+	void Start () {
+		
+        var googleSignInScript = AndroidGoogleSignIn.Init(this.gameObject);
+        googleSignInScript.SignIn(WEB_CLIENT_ID, GoogleSuccessCallback, GoogleErrorCallback);
+
+	}
+
+    void GoogleSuccessCallback(AndroidGoogleSignInAccount s)
+    {
+        squareSprite.color = Color.green;
+
+    }
+
+    void GoogleErrorCallback(string e)
+    {
+        squareSprite.color = Color.red;
+        errorTxt.text = e;
+    }
+}
